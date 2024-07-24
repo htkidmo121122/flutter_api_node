aea9ef0ab954e782cebff86ccd97af9463d3c7c9
Trọng Trần <tranhoangtrong2003@gmail.com>
2024-07-24 11:34:11 +0700

    Chỉnh sửa API local thành 3000

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -259,7 +259,7 @@ const sendResetEmail = (user, token, req) => {
             subject: 'Password Reset',
             text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
                    Please click on the following link, or paste this into your browser to complete the process:\n\n
-                   http://localhost:3002/reset/${token}\n\n
+                   http://localhost:3000/reset/${token}\n\n
                    If you did not request this, please ignore this email and your password will remain unchanged.\n`
         };
 
